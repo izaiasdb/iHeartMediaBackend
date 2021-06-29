56a19fd0599e128d1f1e014567f460562fb52b82
izaiasdb <izaias.dantas@gmail.com>
2021-06-29 13:49:43 -0300

    .env

--- app/config/s3.env.js
+++ app/config/s3.env.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 const env = {
-    AWS_ACCESS_KEY: 'AKIAXLPHS6FUDE2KDU3N',
-    AWS_SECRET_ACCESS_KEY: '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',
-    BUKET: 'iheartmediabucket',
-    REGION: 'us-east-1'
+    AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    BUKET: process.env.BUKET,
+    REGION: process.env.REGION,
 }
 
 module.exports = env;
